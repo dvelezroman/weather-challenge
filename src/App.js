40c65af1d70dc0beb051216b3cb155ec1acf0292
Dario Velez Roman <dvelezroman@gmail.com>
2020-02-28 22:16:06 -0300

    ready for challenge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
-
-// COMPONENTS
-import {Forecast} from './containers/forecast';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-        <Router>
-            <Route exact path="/" component={Forecast} />
-        </Router>
-    );
+    return <div>{"Hello from React"}</div>;
   }
 }
 
--- src/containers/forecast/constants.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = 'f2d0dc77a62f28960f6e71df44c0b81c';
-
-export {
-    API_KEY,
-}
